e98c862aef773e22b3d666d9203e8692e491015d
Professionallance1stt <professionallance1st@gmail.com>
2022-02-24 16:43:36 -0500

    Display User Info

--- app/src/main/java/com/example/mylogin/Settings2.java
+++ app/src/main/java/com/example/mylogin/Settings2.java
@@ -4,46 +4,47 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserInfo;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import org.w3c.dom.Text;
+public class Settings2 extends AppCompatActivity {
 
-public class UserProfile extends AppCompatActivity
-{
+    private static final int RESULT_LOAD_IMAGE = 1;
+
     FirebaseUser user;
     DatabaseReference reference; // user, reference, and userID will be needed to display the user info/email, name.
     String userID;
+
+    ImageView imageToUpload; //Upload image
 
     Button category; // Categories activity button
     Button mySettings; // Settings activity button
     Button myLocation; // set the user location
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_profile);
-        mySettings = (Button) findViewById(R.id.testBtn);
-        myLocation = (Button) findViewById(R.id.location);
-        category = (Button) findViewById(R.id.category);
+        setContentView(R.layout.activity_settings2);
+        imageToUpload = (ImageView) findViewById(R.id.imageToUpload);
         user = FirebaseAuth.getInstance().getCurrentUser(); // gets current user
         reference = FirebaseDatabase.getInstance().getReference("Users"); // the reference get the known data base path "Users"
         userID = user.getUid(); // To get the directory after "Users" you need the userID which is different and automatically generated for each user. From the userID directory, you can access the user data
         final TextView emailAddress = (TextView) findViewById(R.id.EmailTxt); // the final text to display on user profile
         final TextView _name = (TextView) findViewById((R.id.NameTxt));
-
         reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() { // function for displaying user info
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -62,27 +63,22 @@ public class UserProfile extends AppCompatActivity
             }
         });
 
-        mySettings.setOnClickListener(new View.OnClickListener() {
+        imageToUpload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(UserProfile.this, Settings.class));
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
             }
-        });
-        myLocation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(UserProfile.this, MapsActivity.class));
-            }
-        });
-        category.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(UserProfile.this, Categories.class));
-            }
+
         });
 
     }
-
-
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            imageToUpload.setImageURI(selectedImage);
+        }
+    }
 }
