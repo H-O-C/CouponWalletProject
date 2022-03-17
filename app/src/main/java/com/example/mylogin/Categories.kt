415d9a8d2f2702bdfa86c36b7ed6236f18c11d77
Professionallance1stt <professionallance1st@gmail.com>
2022-03-17 15:23:26 -0400

    This is a W.I.P

--- app/src/main/java/com/example/mylogin/Categories.kt
+++ app/src/main/java/com/example/mylogin/Categories.kt
@@ -1,10 +1,11 @@
 package com.example.mylogin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.TextView
-import android.widget.Toast
+import android.view.ViewGroup
+import android.widget.*
 import androidx.recyclerview.widget.GridLayoutManager
 import kotlinx.android.synthetic.main.activity_categories.*
 import retrofit2.Call
@@ -17,11 +18,40 @@ class Categories : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_categories)
     var coupList: MutableList<Offer> = ArrayList()
-        loadCoupons(coupList)
+        val buttondynamic = Button(this)
+        val intent = Intent(this, CouponPage::class.java)
+        loadCoupons(coupList, intent, buttondynamic)
+        val rl = RelativeLayout(this)
+        val llmain = LinearLayout(this)
+
+        /*
+        for (coupon in 0 until coupList.size) {
+            val buttondynamic = Button(this)
+            buttondynamic.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+
+            )
+            buttondynamic.text = coupList[coupon].label
+            buttondynamic.tag = coupList[coupon].code
+            buttondynamic.setOnClickListener()
+            {
+                val intent = Intent(this, CouponPage::class.java)
+                intent.putExtra("Description", coupList[coupon].description)
+                intent.putExtra("Title", coupList[coupon].title)
+                intent.putExtra("CODE", coupList[coupon].code)
+                intent.putExtra("Expiration", coupList[coupon].endDate.toString())
+
+
+                startActivity(intent)
+            }
+
+        }
+*/
 
     }
 
-    private fun loadCoupons(_coupList : MutableList<Offer>) {
+    private fun loadCoupons(_coupList : MutableList<Offer>, _intent : Intent, _button : Button) {
         //initiate the service
         val destinationService = APIBUILDER.buildService(CouponService::class.java)
         val requestCall = destinationService.getAffectedCouponList()
@@ -30,10 +60,29 @@ class Categories : AppCompatActivity() {
             override fun onResponse(call: Call<Coupons>, response: Response<Coupons>) {
                 if (response.isSuccessful) {
                     val couponList = response.body()!!
-                    val couponName = findViewById<TextView>(R.id.testview)
-                    couponName.text = couponList.offers[1].toString()
+                    for (coupon in 0 until couponList.offers.size) {
 
-                } else {
+                        _button.layoutParams = LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.WRAP_CONTENT,
+                            ViewGroup.LayoutParams.WRAP_CONTENT
+
+                        )
+                        _button.text = couponList.offers[coupon].label
+                        _button.tag = couponList.offers[coupon].code
+                        _button.setOnClickListener()
+                        {
+                            _intent.putExtra("Description", couponList.offers[coupon].description)
+                            _intent.putExtra("Title", couponList.offers[coupon].title)
+                            _intent.putExtra("CODE", couponList.offers[coupon].code)
+                            _intent.putExtra("Expiration", couponList.offers[coupon].endDate.toString())
+
+
+                            startActivity(_intent)
+                        }
+
+                    }
+
+                    } else {
                     Toast.makeText(
                         this@Categories,
                         "Something went wrong ${response.message()}",
--- app/src/main/java/com/example/mylogin/Coupons.kt
+++ app/src/main/java/com/example/mylogin/Coupons.kt
@@ -9,3 +9,4 @@ data class Coupons(
     @SerializedName("result")
     val result: Boolean
 )
+
