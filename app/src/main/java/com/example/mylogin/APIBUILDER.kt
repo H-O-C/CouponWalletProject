48baa9e35e67b7477893520e89009dab80de9794
Sean Kramer <SEKramer@student.fullsail.edu>
2022-03-24 20:01:33 -0400

    Got Json reader method implemented

--- app/src/main/java/com/example/mylogin/APIBUILDER.kt
+++ app/src/main/java/com/example/mylogin/APIBUILDER.kt
@@ -3,19 +3,22 @@ package com.example.mylogin
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 import java.util.*
 
 
-object APIBUILDER
-
-{
+object APIBUILDER {
 
     private var random: Random = Random()
     var Check: Int = random.nextInt(16)
 
     private val result = when (Check) {
         in 0..5 -> {
-            "9da784d3351027e3709ea42d8760a92f"
+            "961a03aaba832c863f43abeb7866b1d7"
         }
         in 6..10 -> {
             "3a1ff1fa2224f2a98f326bfaf0661ea8"
@@ -23,12 +26,13 @@ object APIBUILDER
         in 11..15 -> {
             "bf944b0aefbc4ad84264f0c5eab8e7bd"
         }
-        else -> {"9da784d3351027e3709ea42d8760a92f"}
+        else -> {
+            "961a03aaba832c863f43abeb7866b1d7"
+        }
     }
 
 
-
-    private  val apkey = result //The API KEY
+    private val apkey = result //The API KEY
     private const val incremental = true
 
     //$last_extract_datetime = ; // Ideally leave this blank. Populate only if you want to override last extract time stored in the system
@@ -43,6 +47,36 @@ object APIBUILDER
 
     //CREATE HTTP CLIENT
     private val okHttp = OkHttpClient.Builder()
+
+
+    val apiResponse =
+        URL("https://couponapi.org/api/getFeed/?API_KEY=" + apkey + "&incremental=" + incremental + "format=" + format + "off_record=" + off_record).readText()
+
+
+    fun GetJasonData(): String? {
+        return try {
+            val Url =
+                URL("https://couponapi.org/api/getFeed/?API_KEY=" + apkey + "&incremental=" + incremental + "format=" + format + "off_record=" + off_record)
+            val connection: HttpURLConnection = Url.openConnection() as HttpURLConnection
+            val `is`: InputStream = connection.getInputStream()
+            val br = BufferedReader(InputStreamReader(`is`))
+            val sb = StringBuilder()
+            var line: String?
+            while (br.readLine().also { line = it } != null) {
+                sb.append(line)
+            }
+            line = sb.toString()
+            connection.disconnect()
+            `is`.close()
+            sb.delete(0, sb.length)
+            line
+        } catch (e: Exception) {
+          return null
+        }
+    }
+
+
+
 
     //retrofit builder
     private val builder = Retrofit.Builder().baseUrl(URL)
@@ -64,3 +98,5 @@ object APIBUILDER
 
 
 }
+
+
